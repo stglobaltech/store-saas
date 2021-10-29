ffa17760e6a26408d51c8e091f4976d949ac46e8
Mohamed Kasali <mdkasali@gmail.com>
2021-10-29 10:31:04 +0530

    Adds product page with add and update form

--- packages/admin/src/containers/ProductForm/ProductForm.tsx
+++ packages/admin/src/containers/ProductForm/ProductForm.tsx
@@ -1,9 +1,7 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { v4 as uuidv4 } from 'uuid';
-import { useMutation, gql } from '@apollo/client';
 import { Scrollbars } from 'react-custom-scrollbars';
-import { useDrawerDispatch } from 'context/DrawerContext';
+import { useDrawerDispatch, useDrawerState } from 'context/DrawerContext';
 import Uploader from 'components/Uploader/Uploader';
 import Button, { KIND } from 'components/Button/Button';
 import DrawerBox from 'components/DrawerBox/DrawerBox';
@@ -12,7 +10,8 @@ import Input from 'components/Input/Input';
 import { Textarea } from 'components/Textarea/Textarea';
 import Select from 'components/Select/Select';
 import { FormFields, FormLabel } from 'components/FormFields/FormFields';
-
+import { uploadFile } from 'services/REST/restaurant.service';
+import { useQuery } from '@apollo/client';
 import {
   Form,
   DrawerTitleWrapper,
@@ -20,73 +19,17 @@ import {
   FieldDetails,
   ButtonGroup,
 } from '../DrawerItems/DrawerItems.style';
+import {
+  Q_GET_STORE_ID,
+  Q_GET_USER_ID,
+  Q_GET_PARENTRESTAURANTID,
+  Q_GET_CATEGORIES
+} from 'services/GQL';
 
-const options = [
-  { value: 'Fruits & Vegetables', name: 'Fruits & Vegetables', id: '1' },
-  { value: 'Meat & Fish', name: 'Meat & Fish', id: '2' },
-  { value: 'Purse', name: 'Purse', id: '3' },
-  { value: 'Hand bags', name: 'Hand bags', id: '4' },
-  { value: 'Shoulder bags', name: 'Shoulder bags', id: '5' },
-  { value: 'Wallet', name: 'Wallet', id: '6' },
-  { value: 'Laptop bags', name: 'Laptop bags', id: '7' },
-  { value: 'Women Dress', name: 'Women Dress', id: '8' },
-  { value: 'Outer Wear', name: 'Outer Wear', id: '9' },
-  { value: 'Pants', name: 'Pants', id: '10' },
-];
+interface imgUploadRes {[
+  urlText: string
+]: any}
 
-const typeOptions = [
-  { value: 'grocery', name: 'Grocery', id: '1' },
-  { value: 'women-cloths', name: 'Women Cloths', id: '2' },
-  { value: 'bags', name: 'Bags', id: '3' },
-  { value: 'makeup', name: 'Makeup', id: '4' },
-];
-const GET_PRODUCTS = gql`
-  query getProducts(
-    $type: String
-    $sortByPrice: String
-    $searchText: String
-    $offset: Int
-  ) {
-    products(
-      type: $type
-      sortByPrice: $sortByPrice
-      searchText: $searchText
-      offset: $offset
-    ) {
-      items {
-        id
-        name
-        image
-        type
-        price
-        unit
-        salePrice
-        discountInPercent
-      }
-      totalCount
-      hasMore
-    }
-  }
-`;
-const CREATE_PRODUCT = gql`
-  mutation createProduct($product: AddProductInput!) {
-    createProduct(product: $product) {
-      id
-      name
-      image
-      slug
-      type
-      price
-      unit
-      description
-      salePrice
-      discountInPercent
-      # per_unit
-      quantity
-      # creation_date
-    }
-  }
-`;
 type Props = any;
 
 const AddProduct: React.FC<Props> = (props) => {
@@ -94,17 +37,64 @@ const AddProduct: React.FC<Props> = (props) => {
   const closeDrawer = useCallback(() => dispatch({ type: 'CLOSE_DRAWER' }), [
     dispatch,
   ]);
-  const { register, handleSubmit, setValue } = useForm();
-  const [type, setType] = useState([]);
-  const [tag, setTag] = useState([]);
-  const [description, setDescription] = useState('');
 
-  React.useEffect(() => {
-    register({ name: 'type' });
-    register({ name: 'categories' });
-    register({ name: 'image', required: true });
+  const data = useDrawerState('data');
+  const { doCreate, saving } = data;
+
+  const { data: { storeId } } = useQuery(Q_GET_STORE_ID);
+  const { data: { userId } } = useQuery(Q_GET_USER_ID);
+  const { data: { parentRestaurantId } } = useQuery(Q_GET_PARENTRESTAURANTID);
+
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+  const [categories, setCategories] = useState([]);
+  const [productPicture, setProductPicture] = useState({
+    url: undefined,
+    uploading: false
+  });
+  const [description, setDescription] = useState('');
+  const [priceSplit, setPriceSplit] = useState({
+    price: "0",
+    priceWithoutVat: "0",
+    vatPrice: "0"
+  });
+  const [category, setCategory] = useState([]);
+
+  const { error } = useQuery(Q_GET_CATEGORIES, {
+    variables: { storeId },
+    onCompleted: ({ getCategories }) => {
+      const { productCategories } = getCategories;
+      if(productCategories.length) {
+        const categoryOptions = productCategories.map((option) => {
+          return { label: option.name.en, value: option._id };
+        });
+
+        setCategories(categoryOptions);
+      }
+    }
+  });
+
+  useEffect(() => {
     register({ name: 'description' });
+    register('category', { required: true });
   }, [register]);
+
+  const uploadImage = (files) => {
+    setProductPicture({ ...productPicture, uploading: true });
+    const formData = new FormData();
+    formData.append("file", files[0], files[0].name);
+    uploadFile(formData)
+      .then((result: imgUploadRes) => {
+        setProductPicture({
+          ...productPicture,
+          url: result.urlText,
+          uploading: false,
+        });
+      })
+      .catch((err) => {
+        console.error(err, 'on image upload for store logo');
+        setProductPicture({ ...productPicture, uploading: false });
+      });
+  };
 
   const handleDescriptionChange = (e) => {
     const value = e.target.value;
@@ -112,58 +102,54 @@ const AddProduct: React.FC<Props> = (props) => {
     setDescription(value);
   };
 
-  const [createProduct] = useMutation(CREATE_PRODUCT, {
-    update(cache, { data: { createProduct } }) {
-      const { products } = cache.readQuery({
-        query: GET_PRODUCTS,
-      });
+  const handlePriceChange = (e) => {
+    let price, priceWithoutVat, vatPrice;
+    price = Number(e.target.value);
+    if (price > 0) {
+      priceWithoutVat = (price - price * 0.05).toFixed(2);
+      vatPrice = (price - priceWithoutVat).toFixed(2);
+      setPriceSplit({ price, priceWithoutVat, vatPrice });
+    } else {
+      setPriceSplit({ price: "0", priceWithoutVat: "0", vatPrice: "0" });
+    }
+  }
 
-      cache.writeQuery({
-        query: GET_PRODUCTS,
-        data: {
-          products: {
-            __typename: products.__typename,
-            items: [createProduct, ...products.items],
-            hasMore: true,
-            totalCount: products.items.length + 1,
-          },
-        },
-      });
-    },
-  });
-  const handleMultiChange = ({ value }) => {
-    setValue('categories', value);
-    setTag(value);
+  const handleCategoryChange = ({ value }) => {
+    setValue('category', value);
+    setCategory(value);
   };
 
-  const handleTypeChange = ({ value }) => {
-    setValue('type', value);
-    setType(value);
+  const onSubmit = (values) => {
+    const productCreateInput = {
+      productName: {
+        en: values.productName,
+        ar: ""
+      },
+      price: {
+        price: parseFloat(priceSplit.price),
+        basePrice: parseFloat(priceSplit.priceWithoutVat),
+        vatPrice: parseFloat(priceSplit.vatPrice)
+      },
+      maxQuantity: parseFloat(values.quantity),
+      description: {
+        en: description,
+        ar: ""
+      },
+      picture: productPicture.url ? productPicture.url : "https://restaurant-shafeer.s3.ap-south-1.amazonaws.com/store/product-pic/product-pic-1593153655693.png",
+      categoryId: category[0].value,
+      payType: "any",
+      ownerId: userId,
+      parentStoreCode: parentRestaurantId,
+      originStoreCode: storeId,
+      isAvailable: true
+    }
+  
+    doCreate({ variables: { productCreateInput } });
   };
-  const handleUploader = (files) => {
-    setValue('image', files[0].path);
-  };
-  const onSubmit = (data) => {
-    const newProduct = {
-      id: uuidv4(),
-      name: data.name,
-      type: data.type[0].value,
-      description: data.description,
-      image: data.image && data.image.length !== 0 ? data.image : '',
-      price: Number(data.price),
-      unit: data.unit,
-      salePrice: Number(data.salePrice),
-      discountInPercent: Number(data.discountInPercent),
-      quantity: Number(data.quantity),
-      slug: data.name,
-      creation_date: new Date(),
-    };
-    console.log(newProduct, 'newProduct data');
-    createProduct({
-      variables: { product: newProduct },
-    });
-    closeDrawer();
-  };
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
 
   return (
     <>
@@ -206,7 +192,7 @@ const AddProduct: React.FC<Props> = (props) => {
                   },
                 }}
               >
-                <Uploader onChange={handleUploader} />
+                <Uploader onChange={uploadImage} />
               </DrawerBox>
             </Col>
           </Row>
@@ -221,11 +207,28 @@ const AddProduct: React.FC<Props> = (props) => {
             <Col lg={8}>
               <DrawerBox>
                 <FormFields>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel>Product Name</FormLabel>
                   <Input
-                    inputRef={register({ required: true, maxLength: 20 })}
-                    name="name"
+                    name="productName"
+                    inputRef={register({ required: true, minLength: 3, maxLength: 20 })}
                   />
+                  {errors.productName && (
+                    <div
+                      style={{
+                        margin: "5px 0 0 auto",
+                        fontFamily: "Lato, sans-serif",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        color: "rgb(252, 92, 99)",
+                      }}
+                    >
+                      {errors.productName.type === "required"
+                        ? "Required"
+                        : (errors.productName.type === "minLength" ||
+                            errors.productName.type === "maxLength") &&
+                          "Product Name must be 3-20 characters"}
+                    </div>
+                  )}
                 </FormFields>
 
                 <FormFields>
@@ -237,52 +240,56 @@ const AddProduct: React.FC<Props> = (props) => {
                 </FormFields>
 
                 <FormFields>
-                  <FormLabel>Unit</FormLabel>
-                  <Input type="text" inputRef={register} name="unit" />
-                </FormFields>
-
-                <FormFields>
                   <FormLabel>Price</FormLabel>
                   <Input
                     type="number"
-                    inputRef={register({ required: true })}
                     name="price"
-                  />
-                </FormFields>
-
-                <FormFields>
-                  <FormLabel>Sale Price</FormLabel>
-                  <Input type="number" inputRef={register} name="salePrice" />
-                </FormFields>
-
-                <FormFields>
-                  <FormLabel>Discount In Percent</FormLabel>
-                  <Input
-                    type="number"
                     inputRef={register}
-                    name="discountInPercent"
+                    onChange={handlePriceChange}
                   />
                 </FormFields>
 
                 <FormFields>
-                  <FormLabel>Product Quantity</FormLabel>
+                  <FormLabel>Max Quantity</FormLabel>
                   <Input
                     type="number"
-                    inputRef={register({ required: true })}
                     name="quantity"
+                    inputRef={register({
+                      required: true,
+                      validate: (value) =>
+                        value > 0 ? true : "invalid maxQuantity"
+                    })}
                   />
+                  {errors.quantity && (
+                    <div
+                      style={{
+                        margin: "5px 0 0 auto",
+                        fontFamily: "Lato, sans-serif",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        color: "rgb(252, 92, 99)",
+                      }}
+                    >
+                      {errors.quantity.type === "required"
+                        ? "Required"
+                        : (errors.quantity.message === "invalid maxQuantity" &&
+                          "Max quantity must be greater than zero")
+                      }
+                    </div>
+                  )}
                 </FormFields>
 
                 <FormFields>
-                  <FormLabel>Type</FormLabel>
+                  <FormLabel>Category</FormLabel>
                   <Select
-                    options={typeOptions}
-                    labelKey="name"
+                    options={categories}
+                    labelKey="label"
                     valueKey="value"
-                    placeholder="Product Type"
-                    value={type}
+                    placeholder="Category"
+                    value={category}
                     searchable={false}
-                    onChange={handleTypeChange}
+                    clearable={false}
+                    onChange={handleCategoryChange}
                     overrides={{
                       Placeholder: {
                         style: ({ $theme }) => {
@@ -329,46 +336,19 @@ const AddProduct: React.FC<Props> = (props) => {
                       },
                     }}
                   />
-                </FormFields>
-
-                <FormFields>
-                  <FormLabel>Categories</FormLabel>
-                  <Select
-                    options={options}
-                    labelKey="name"
-                    valueKey="value"
-                    placeholder="Product Tag"
-                    value={tag}
-                    onChange={handleMultiChange}
-                    overrides={{
-                      Placeholder: {
-                        style: ({ $theme }) => {
-                          return {
-                            ...$theme.typography.fontBold14,
-                            color: $theme.colors.textNormal,
-                          };
-                        },
-                      },
-                      DropdownListItem: {
-                        style: ({ $theme }) => {
-                          return {
-                            ...$theme.typography.fontBold14,
-                            color: $theme.colors.textNormal,
-                          };
-                        },
-                      },
-                      Popover: {
-                        props: {
-                          overrides: {
-                            Body: {
-                              style: { zIndex: 5 },
-                            },
-                          },
-                        },
-                      },
-                    }}
-                    multi
-                  />
+                  {errors.category && (
+                    <div
+                      style={{
+                        margin: "5px 0 0 auto",
+                        fontFamily: "Lato, sans-serif",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                        color: "rgb(252, 92, 99)",
+                      }}
+                    >
+                      {errors.category.type === "required" && "Required" }
+                    </div>
+                  )}
                 </FormFields>
               </DrawerBox>
             </Col>
@@ -377,6 +357,7 @@ const AddProduct: React.FC<Props> = (props) => {
 
         <ButtonGroup>
           <Button
+            type="button"
             kind={KIND.minimal}
             onClick={closeDrawer}
             overrides={{
@@ -398,6 +379,7 @@ const AddProduct: React.FC<Props> = (props) => {
 
           <Button
             type="submit"
+            disabled={saving || productPicture.uploading}
             overrides={{
               BaseButton: {
                 style: ({ $theme }) => ({
@@ -410,7 +392,7 @@ const AddProduct: React.FC<Props> = (props) => {
               },
             }}
           >
-            Create Product
+            {saving ? "Saving" : "Create Product"}
           </Button>
         </ButtonGroup>
       </Form>
