e6e0c3a7068ab75928dcc054ce2a34b046e68395
Aboobakker <siddheeq@tmmt.com>
2021-11-02 12:40:22 +0530

    removed duplicate getProductsBasedOnStore query file

--- packages/admin/src/services/GQL/query/index.js
+++ packages/admin/src/services/GQL/query/index.js
@@ -1,7 +1,6 @@
 export * from './logged-in.query';
 export * from './restaurant';
 export * from './productCategories.query';
-export * from './getProductsBasedOnStore.query';
 export * from './workFlowPolicy';
 export * from './orders';
 export * from './products/products';
